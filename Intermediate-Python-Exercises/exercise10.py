7fe63dd4b29e1e258ff36dfe0b8c5188974697ba
debian <debian@debian.debian>
2026-05-02 14:12:04 -0400

    Fix: exercise10 and exercise16 - exercise10 fix index error - exercise16 contains empty set

--- Intermediate-Python-Exercises/exercise10.py
+++ Intermediate-Python-Exercises/exercise10.py
@@ -3,7 +3,8 @@ def exercise10(lst: list, n: int, direction: str) -> list:
     if direction == 'right':
         lst = lst[-n:] + lst[:-n] 
     elif direction == 'left':
-        lst = lst[n+1:] + lst[:n+1]
+        lst = lst[n:] + lst[:n]
     return lst
 if __name__ == "__main__":
     print(exercise10([1, 2, 3, 4, 5], 2, 'right'))
+    print(exercise10([1, 2, 3, 4, 5], 2, 'left'))
