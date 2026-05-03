36c538037943e377cac6a287f5f6cd6ab63a087a
debian <debian@debian.debian>
2026-05-03 14:16:27 -0400

    Fix: fix exercise 17 - Use datetime not date on now()

--- Intermediate-Python-Exercises/exercise17.py
+++ Intermediate-Python-Exercises/exercise17.py
@@ -1,8 +1,9 @@
-from datetime import date
+from datetime import date, datetime
 from dateutil.relativedelta import relativedelta
 def exercise17(birthday: str, today: str = "") -> str: 
-    return f"Age: {(age := relativedelta(date.fromisoformat(today) if today else date.now(), date.fromisoformat(birthday)) )
+    return f"Age: {(age := relativedelta(date.fromisoformat(today) if today else datetime.now(), date.fromisoformat(birthday)) )
 .years} years, {age.months} months, {age.days} days" 
 
 if __name__ == "__main__":
     print(exercise17("1995-05-15","2026-01-02"))
+    print(exercise17("1995-05-15"))
